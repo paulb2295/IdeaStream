167f19496be3fe0a7709f4bbcde64cbb2384128f
paulb2295 <borleapaul@gmail.com>
2025-09-14 17:11:05 +0300

    templates done

--- idea_stream/admin.py
+++ idea_stream/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from idea_stream.models import UserProfile, Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "word_count", "status", "created_at", "updated_at")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    date_hierarchy = "created_at"
+    ordering = ("created_at",)
+    readonly_fields = ("word_count", "created_at", "updated_at")
+
+
 admin.site.register(UserProfile)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
--- idea_stream/models.py
+++ idea_stream/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -15,7 +17,7 @@ class UserProfile(AbstractUser):
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default='')
-    word_count = models.IntegerField()
+    word_count = models.IntegerField(blank=True, default="")
     x_post = models.TextField(blank=True, default='')
     status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default='draft')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -23,3 +25,8 @@ class Article(models.Model):
 
     def __str__(self):
         return f"{self.title} | {self.status}"
+
+    def save(self, *args, **kwargs):
+        text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
+        self.word_count = len(re.findall(r"\b\w+\b", text))
+        super().save(*args, **kwargs)
